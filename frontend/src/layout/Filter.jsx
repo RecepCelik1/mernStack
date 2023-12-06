64b1b433836368bd8a41b9f973cceedd5bb4c016
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-12-06 18:02:51 +0300

    v1

--- frontend/src/layout/Filter.jsx
+++ frontend/src/layout/Filter.jsx
@@ -1,29 +1,44 @@
+//=> filtreleme kısımlarının backende istek olarak gönderilmesi için kullanıcıdan gerekli parametrelerin alındığı kısım
+
 import React from "react";
 
+
+
 const Filter = ({setPrice, setRating, setCategory}) => {
+
+    //=> filtreleme seçenekleri
     const categoryList = [
         "Çanta" , "Ayakkabı" , "Bilgisayar" , "Telefon" , "Pantolon"
     ]
     const ratingList = [
         1,2,3,4,5
     ]
+
+
     return (
         <div className="w-[200px] mt-3 p-1">
             <div>Filtreleme</div>
             <div className="flex items-center gap-2">
 
+            {/*min-max fiyat parametrelerinin alınıp floata dönüştürüldüğü kısımdır. sayıya dönüşen parametre setPrice aracılığı 
+            ile prev durumu içinde güncellenir*/}
             <input onChange={e => setPrice(prev => ({...prev, min: parseFloat(e.target.value)}))} className="border w-16 p-1 outline-none my-2" type="number" placeholder="Min"/>
             <input onChange={e => setPrice(prev => ({...prev, max: parseFloat(e.target.value)}))} className="border w-16 p-1 outline-none my-2" type="number" placeholder="Max"/>
 
 
             </div>
+
+            {/* yukarıda tanımladığımı çanta ayakkabı, rating fln burada map edilir */}
             <div className="my-2">Kategori</div>
+            
             {
                 categoryList.map((category, i) => (
                     <div onClick={() => setCategory(category)} className="text-sm cursor-pointer" key={i}>{category}</div>
                 )) 
             }
+
             <hr />
+
             <div className="my-2">Puanlama</div>
             {
                 ratingList.map((rating, i) => (
--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -1,3 +1,5 @@
+//=> burası header bölümü için
+
 import React, { useState } from "react";
 import { SlBasket } from "react-icons/sl";
 import { IoIosSearch } from "react-icons/io";
@@ -7,59 +9,82 @@ import { getKeyword } from "../redux/generalSlice";
 
 
 const Header = () => {
-
-    const [openMenu, setOpenMenu] = useState(false)
-    const [keyword, setKeyword] = useState("")
+    const [openMenu, setOpenMenu] = useState(false);
+    const [keyword, setKeyword] = useState("");
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const menuItems = [
-        {   
+        {
             name: "Profil",
-            url: "/profile"
+            url: "/profile",
         },
         {
             name: "Admin",
-            url: "/admin"
+            url: "/admin",
         },
         {
             name: "Çıkış Yap",
-            url: "/logout"
-        }
-    ]
+            url: "/logout",
+        },
+    ];
 
-    const keywordFunc = () => {
+    const keywordFunc = () => { //=> kullanıcı tarafından girilen anahtar kelimenin tutulması ve store.js e dispatch edilmesi
         console.log(keyword);
-        dispatch(getKeyword(keyword))
-        navigate('/products')
-        setKeyword('')
-    }
+        dispatch(getKeyword(keyword));
+        setKeyword('');
+        navigate('/products');
+    };
 
-    return(
+    return (
         <div className='bg-gray-300 h-16 px-2 flex items-center justify-between'>
+
+
             <div className='text-4xl'>
-                mern stack project
+                MERN Stack Project
             </div>
+
+
             <div className='flex items-center gap-5'>
+
+
                 <div className="flex items-center">
-                    <input value={keyword} onChange={e => setKeyword(e.target.value)} className="p-2 outline-none" type="text" placeholder="Arama yap"/>
+
+                    {/* aramanın yapıldığı kısım ve onchance ile setKeyword tetiklenip girilen parametre keyword içerisinde tutulur */}
+                    <input value={keyword} onChange={e => setKeyword(e.target.value)} className="p-2 outline-none" type="text" 
+                    placeholder="Arama yap"/>
+                    {/* onClik ile keywordFunc tetiklenir ve backendden o keyword ile filtreleme istediğinde bulunulur */}
                     <div onClick={keywordFunc} className="p-2 ml-1 cursor-pointer"><IoIosSearch size={30}/></div>
+                
                 </div>
+
+
                 <div className='relative'>
-                <img onClick={()=> setOpenMenu(!openMenu)} className="w-8 h-8 rounded-full" src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg" alt="" />
-                {openMenu && <div className='absolute right-0 mt-3 w-[200px] bg-white shadow-lg shadow-gray-300'>
-                    {menuItems.map((item, i) => (
-                        <div className="px-2 py-1 hover:bg-gray-200 " key={i}>{item.name}</div>
-                    ))}
-                </div>}
+
+                {/* kullanıcı avatarı ve menüleri gömülmüştür onClick edildiğinde setOpenMenu fonksiyonu tetiklenir ve menü maplanir */}
+                    <img onClick={()=> setOpenMenu(!openMenu)} className="w-8 h-8 rounded-full" src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg" alt="" />
+                    {openMenu && <div className='absolute right-0 mt-3 w-[200px] bg-white shadow-lg shadow-gray-300'>
+                        {menuItems.map((item, i) => (
+                            <div className="px-2 py-1 hover:bg-gray-200 " key={i} onClick={() => navigate(item.url)}>{item.name}</div>
+                        ))}
+                    </div>}
+
                 </div>
+
+
                 <div className='relative'>
+                    {/* sepet kısmı */}
                     <SlBasket size={30}/>
                     <div className="absolute -top-2 -right-2 w-5 h-5 bg-red-500 rounded-full flex items-center justify-center">4</div>
+                
                 </div>
+            
+            
             </div>
+        
+        
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
